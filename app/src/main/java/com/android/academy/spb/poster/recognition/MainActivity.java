afe61de90dc9bb1feed551902c5be92e508d202c
v.markus <v.markus@tinkoff.ru>
2018-06-10 22:53:21 +0300

    Add Button

---
+++ app/src/main/java/com/android/academy/spb/poster/recognition/MainActivity.java
@@ -0,0 +1,18 @@
+package com.android.academy.spb.poster.recognition;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author Vitaliy Markus
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
