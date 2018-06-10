cadc0d2b147f898d5ecb87fb8bac71e54696a5e6
Anastasia Kavunskaya <kavunskanas@gmail.com>
2018-06-10 23:18:58 +0300

    mini update

---
+++ app/src/main/java/com/android/academy/spb/poster/recognition/StartActivity.java
@@ -0,0 +1,26 @@
+package com.android.academy.spb.poster.recognition;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * @author Vitaliy Markus
+ */
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartActivity.this, MainActivity.class));
+            }
+        });
+    }
+}
